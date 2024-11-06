a2f6bc95d873f707f554d4191a62d2afee8f35bf
José Henrique <124707974+Oliveirahenriq@users.noreply.github.com>
2024-11-05 23:03:54 -0300

    Add files via upload

---
+++ Avaliação_de_arquiteturas/camadas/camada_negocios.py
@@ -0,0 +1,37 @@
+# camada_negocios.py
+
+from camada_dados import CamadaDados
+
+class CamadaNegocios:
+    def __init__(self):
+        self.dados = CamadaDados()
+
+    def adicionar_contato(self, nome, telefone, email):
+        if not nome or not telefone or not email:
+            raise ValueError("Nome, telefone e email são obrigatórios")
+        return self.dados.adicionar_contato(nome, telefone, email)
+
+    def adicionar_compromisso(self, descricao, data, contato_id=None):
+        if not descricao or not data:
+            raise ValueError("Descrição e data são obrigatórios")
+        return self.dados.adicionar_compromisso(descricao, data, contato_id)
+
+    def listar_contatos(self):
+        return self.dados.listar_contatos()
+
+    def listar_compromissos(self):
+        compromissos = self.dados.listar_compromissos()
+        return [
+            {
+                'id': c[0],
+                'descricao': c[1],
+                'data': c[2],
+                'contato': {'id': c[3], 'nome': c[4]} if c[3] else None
+            }
+            for c in compromissos
+        ]
+    
+    def pesquisar_compromissos_por_intervalo(self, data_inicio, data_fim):
+        if not data_inicio or not data_fim:
+            raise ValueError ("As datas de início e fim são obrigatórias")
+        return self.dados.buscar_compromissos_por_intervalo(data_fim, data_fim)
---
+++ Avaliação_de_arquiteturas/camadas/main.py
@@ -0,0 +1,7 @@
+# main.py
+
+from camada_apresentacao import CamadaApresentacao
+
+if __name__ == "__main__":
+    app = CamadaApresentacao()
+    app.executar()
